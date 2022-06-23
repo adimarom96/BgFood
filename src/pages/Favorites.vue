b67dc89922e4f01e39e4cf6a6c122df617fd4ad4
Adi-Marom <adimaro@bgu.ac.il>
2022-06-23 16:03:14 +0300

    about file

---
+++ src/pages/Favorites.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="container">
+  <h1> Favorites</h1>
+    <RecipePreviewList
+      v-if="this.clicked"
+      title="Favorites"
+      state="favorites"
+      class="RandomRecipes center"
+    />
+  </div>
+</template>
+
+<script>
+import RecipePreviewList from "../components/RecipePreviewList";
+
+export default {
+  components: {
+    RecipePreviewList,
+  },
+
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style></style>
